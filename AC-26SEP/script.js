0a97fc8ab014b84040e4c4a120b341f0e11ab7d8
Rira-Jireth González Hernández <133671942+RiraEverdeen@users.noreply.github.com>
2024-09-28 13:35:08 -0500

    Add files via upload
    Se adopto las clases Linea, Circunferencia, y elipse para que manejen correctamente el nuevo Punto encapsulado

--- AC-26SEP/script.js
+++ AC-26SEP/script.js
@@ -1,31 +1,57 @@
-// Clase Punto (con encapsulamiento usando #)
-class Punto {
-    #x1;
-    #y1;
-    #x2;
-    #y2;
-
-    constructor(x1, y1, x2 = null, y2 = null) {
-        this.#x1 = x1;
-        this.#y1 = y1;
-        this.#x2 = x2;
-        this.#y2 = y2;
+// Clase Linea (en SVG) usa la clase Punto
+class Linea {
+    constructor(punto) {
+        this._punto = punto; // Utilizando un objeto de tipo Punto
     }
 
-    // Métodos para obtener los valores de las coordenadas
-    getX1() {
-        return this.#x1;
+    // Método para dibujar la línea en SVG
+    dibujar(svg) {
+        const linea = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        linea.setAttribute("x1", this._punto.getX1());
+        linea.setAttribute("y1", this._punto.getY1());
+        linea.setAttribute("x2", this._punto.getX2());
+        linea.setAttribute("y2", this._punto.getY2());
+        linea.setAttribute("stroke", "black");
+        svg.appendChild(linea);
+    }
+}
+
+// Clase Circunferencia (en SVG) que usa la clase Punto para el centro
+class Circunferencia {
+    constructor(centro, radio) {
+        this._centro = centro; // Objeto de la clase Punto
+        this._r = radio;
     }
 
-    getY1() {
-        return this.#y1;
+    // Método para dibujar la circunferencia en SVG
+    dibujar(svg) {
+        const circunferencia = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circunferencia.setAttribute("cx", this._centro.getX1());
+        circunferencia.setAttribute("cy", this._centro.getY1());
+        circunferencia.setAttribute("r", this._r);
+        circunferencia.setAttribute("stroke", "black");
+        circunferencia.setAttribute("fill", "none");
+        svg.appendChild(circunferencia);
+    }
+}
+
+// Clase Elipse (en SVG) que usa la clase Punto para el centro
+class Elipse {
+    constructor(centro, radioX, radioY) {
+        this._centro = centro; // Objeto de la clase Punto
+        this._rx = radioX;
+        this._ry = radioY;
     }
 
-    getX2() {
-        return this.#x2;
-    }
-
-    getY2() {
-        return this.#y2;
+    // Método para dibujar la elipse en SVG
+    dibujar(svg) {
+        const elipse = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
+        elipse.setAttribute("cx", this._centro.getX1());
+        elipse.setAttribute("cy", this._centro.getY1());
+        elipse.setAttribute("rx", this._rx);
+        elipse.setAttribute("ry", this._ry);
+        elipse.setAttribute("stroke", "black");
+        elipse.setAttribute("fill", "none");
+        svg.appendChild(elipse);
     }
 }
